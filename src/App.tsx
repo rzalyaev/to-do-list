829f1ecf657e9458d295834974db6b79f68547f1
rzalyaev <ruslan.zalyaev@gmail.com>
2023-05-09 23:47:09 +0400

    Add addTask, removeTask and changeTaskStatus functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,49 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { v1 } from 'uuid';
 import './App.css';
-import {Todolist} from "./Todolist";
+import {Todolist} from './components/Todolist/Todolist';
+
+export type TaskType = {
+    id: string
+    title: string
+    isDone: boolean
+}
+
+export type FilterType = 'all' | 'active' | 'completed'
 
 function App() {
-    const tasks1 = [
-        { id: 1, title: "HTML&CSS", isDone: true },
-        { id: 2, title: "JS", isDone: true },
-        { id: 3, title: "ReactJS", isDone: false },
-        { id: 4, title: "ReactJS", isDone: false }
-    ]
-    const tasks2 = [
-        { id: 1, title: "Hello world", isDone: true },
-        { id: 2, title: "I am Happy", isDone: false },
-        { id: 3, title: "Yo", isDone: false }
-    ]
+    const [tasks, setTasks] = useState<TaskType[]>([
+        { id: v1(), title: "HTML&CSS", isDone: true },
+        { id: v1(), title: "JS", isDone: true },
+        { id: v1(), title: "ReactJS", isDone: false },
+        { id: v1(), title: "Redux", isDone: true },
+        { id: v1(), title: "ReactQuery", isDone: false }
+    ])
+
+    const addTask = (title: string) => {
+        let task = {id: v1(), title: title, isDone: false};
+        setTasks([task, ...tasks]);
+    }
+
+    const removeTask = (taskId: string) => {
+        setTasks(tasks.filter(el => el.id !== taskId));
+    }
+
+    const changeTaskStatus = (id: string, isDone: boolean) => {
+        let task = tasks.find(task => task.id === id);
+        if (task) {
+            task.isDone = isDone;
+            setTasks([...tasks]);
+        }
+    }
+
     return (
-        <>
-            <Todolist header={'What to do'} track={100200} tasks={tasks1}/>
-            <Todolist header={'What to learn'} tasks={tasks2}/>
-        </>
+        <div className="App">
+            <Todolist title="What to learn"
+                      tasks={tasks}
+                      addTask={addTask}
+                      removeTask={removeTask} changeTaskStatus={changeTaskStatus}/>
+        </div>
     );
 }
 
