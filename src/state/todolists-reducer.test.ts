eb2eef47c401b01327a5c313fb3d8d50020a8528
rzalyaev <ruslan.zalyaev@gmail.com>
2023-06-20 23:19:34 +0400

    Add test for todolist filter changing

--- src/state/todolists-reducer.test.ts
+++ src/state/todolists-reducer.test.ts
@@ -1,6 +1,6 @@
 import { todolistsReducer } from './todolists-reducer'
 import { v1 } from 'uuid'
-import { TodolistType } from '../App'
+import {FilterType, TodolistType} from '../App'
 
 test('correct todolist should be removed', () => {
     let todolistId1 = v1()
@@ -55,4 +55,27 @@ test('correct todolist should change its title', () => {
 
     expect(endState[0].title).toBe('What to learn')
     expect(endState[1].title).toBe(newTodolistTitle)
+})
+
+test('correct filter of todolist should be changed', () => {
+    let todolistId1 = v1()
+    let todolistId2 = v1()
+
+    let newFilter: FilterType = 'completed'
+
+    const startState: Array<TodolistType> = [
+        {id: todolistId1, title: 'What to learn', filter: 'all'},
+        {id: todolistId2, title: 'What to buy', filter: 'all'}
+    ]
+
+    const action = {
+        type: 'CHANGE-TODOLIST-FILTER',
+        id: todolistId2,
+        filter: newFilter
+    }
+
+    const endState = todolistsReducer(startState, action)
+
+    expect(endState[0].filter).toBe('all')
+    expect(endState[1].filter).toBe(newFilter)
 })
