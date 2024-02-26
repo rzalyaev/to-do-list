edeb898770502179b9fc672a74393a6f01961998
rzalyaev <ruslan.zalyaev@gmail.com>
2024-02-26 14:08:37 +0400

    Create Todolist component

---
+++ src/components/Todolist.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {TasksType} from "../App";
+import {Button} from "./Button/Button";
+
+type TodolistType = {
+  title: string
+  tasks: TasksType
+  date?: string
+}
+
+export const Todolist = ({title, tasks, date}: TodolistType) => {
+  const tasksList = tasks.map(task => {
+    return <li key={task.id}><input type="checkbox" checked={task.isDone}/> <span>{task.title}</span></li>
+  })
+
+  return (
+      <div>
+        <h3>{title}</h3>
+        <div>
+          <input/>
+          <Button title={"+"}/>
+        </div>
+        <ul>
+          {tasksList.length > 0 ? tasksList : "There is no task"}
+        </ul>
+        <div>
+          <Button title={"All"}/>
+          <Button title={"Active"}/>
+          <Button title={"Completed"}/>
+        </div>
+        <div>{date}</div>
+      </div>
+  );
+};
