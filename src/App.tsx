805046b1daad40b36d4c20552cd1384cdf3caefa
rzalyaev <ruslan.zalyaev@gmail.com>
2024-02-26 14:10:28 +0400

    Add tasks data

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,17 @@ type TaskType = {
 export type TasksType = TaskType[]
 
 function App() {
+  const tasks1: TasksType = [
+    {id: 1, title: "HTML&CSS", isDone: true},
+    {id: 2, title: "JS", isDone: true},
+    {id: 3, title: "ReactJS", isDone: false},
+    {id: 4, title: 'Redux', isDone: false},
+    {id: 5, title: 'TypeScript', isDone: false},
+    {id: 6, title: 'RTK Query', isDone: false}
+  ]
+
+  const tasks2: TasksType = []
+
   return (
     <div className="App">
       <header className="App-header">
