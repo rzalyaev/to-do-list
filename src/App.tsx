a87db75cc4340b5add194c6280933e522e2508f7
rzalyaev <ruslan.zalyaev@gmail.com>
2024-02-26 14:10:50 +0400

    Remove basic code, add Todolist components

--- src/App.tsx
+++ src/App.tsx
@@ -23,22 +23,10 @@ function App() {
   const tasks2: TasksType = []
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <Todolist title={"What to learn"} tasks={tasks1} date={"26.02.2024"}/>
+        <Todolist title={"Songs"} tasks={tasks2}/>
+      </div>
   );
 }
 
