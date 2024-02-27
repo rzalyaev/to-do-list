45c520a85e21f3589ea3220a0dd13525531bd771
rzalyaev <ruslan.zalyaev@gmail.com>
2024-02-27 16:49:09 +0400

    Add filtration functionality

--- src/components/Todolist.tsx
+++ src/components/Todolist.tsx
@@ -14,6 +14,25 @@ export type FilterMethodType = 'All' | 'Active' | 'Completed'
 
 export const Todolist = ({title, tasks, date}: TodolistType) => {
   const [currentTasks, setCurrentTasks] = useState<TasksType>(tasks);
+  // Filtration --------------------------------------------------------------------------------------------------------
+  const [filterMethod, setFilterMethod] = useState<FilterMethodType>('All');
+  const filterTasks = (filterMethod: FilterMethodType) => {
+    if (filterMethod === 'Active') {
+      return currentTasks.filter(task => !task.isDone);
+    }
+    if (filterMethod === 'Completed') {
+      return currentTasks.filter(task => task.isDone);
+    }
+    return currentTasks;
+  }
+  const changeFilterMethod = (newFilterMethod: FilterMethodType) => {
+    setFilterMethod(newFilterMethod);
+    filterTasks(filterMethod);
+  }
+  const changeFilterMethodToAll = () => changeFilterMethod('All');
+  const changeFilterMethodToActive = () => changeFilterMethod('Active');
+  const changeFilterMethodToCompleted = () => changeFilterMethod('Completed');
+
   // Tasks mapping -----------------------------------------------------------------------------------------------------
   const tasksList = filterTasks(filterMethod).map(task => {
     return (
@@ -36,9 +55,9 @@ export const Todolist = ({title, tasks, date}: TodolistType) => {
           {tasksList.length > 0 ? tasksList : "There is no task"}
         </ul>
         <div>
-          <Button title={"All"}/>
-          <Button title={"Active"}/>
-          <Button title={"Completed"}/>
+          <Button title={"All"} onClick={changeFilterMethodToAll}/>
+          <Button title={"Active"} onClick={changeFilterMethodToActive}/>
+          <Button title={"Completed"} onClick={changeFilterMethodToCompleted}/>
         </div>
         <div>{date}</div>
       </div>
