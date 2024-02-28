0a2e138d0300d1cccebdd01789fca888e2affcb3
rzalyaev <ruslan.zalyaev@gmail.com>
2024-02-29 00:12:52 +0400

    Add task addition by press enter

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,14 +1,19 @@
-import React, {ChangeEvent} from 'react';
+import React, {ChangeEvent, KeyboardEvent} from 'react';
 
 type InputPropsType = {
   type: string
   value?: string
   checked?: boolean
   onChangeHandler: (event: ChangeEvent<HTMLInputElement>) => void
+  onKeyUpHandler?: (event: KeyboardEvent<HTMLInputElement>) => void
 }
 
-export const Input = ({type, value, onChangeHandler, checked}: InputPropsType) => {
+export const Input = ({type, value, checked, onChangeHandler, onKeyUpHandler}: InputPropsType) => {
   return (
-      <input type={type} value={value} onChange={onChangeHandler} checked={checked}/>
+      <input type={type}
+             value={value}
+             onChange={onChangeHandler}
+             checked={checked} onKeyDown={onKeyUpHandler}
+      />
   );
 };
