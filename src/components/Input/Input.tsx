f05b9d96af4838c55236c765676522e6bed637b1
rzalyaev <ruslan.zalyaev@gmail.com>
2024-03-06 23:20:07 +0400

    Refactor input props

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,26 +1,32 @@
 import React, {ChangeEvent, KeyboardEvent} from 'react';
 import styles from './Input.module.css';
 
-type InputPropsType = {
+export type InputPropsType = {
   type: string
   value?: string
   checked?: boolean
   onChangeHandler: (event: ChangeEvent<HTMLInputElement>) => void
   onKeyUpHandler?: (event: KeyboardEvent<HTMLInputElement>) => void
-  className?: string
+  onBlurHandler?: () => void
+  inputClassName?: string
   placeholder?: string
 }
 
-export const Input = ({type, value, checked, onChangeHandler, onKeyUpHandler, className, placeholder}: InputPropsType) => {
-  const inputClassName: string = `${styles.input} ${className ? className : ''}`;
+export const Input = ({
+                        type, value, checked, onChangeHandler, onKeyUpHandler, onBlurHandler, inputClassName, placeholder
+}: InputPropsType) => {
+  const inputFinalClassName: string = `${styles.input} ${inputClassName ? inputClassName : ''}`;
 
   return (
       <input type={type}
              value={value}
+             checked={checked}
              onChange={onChangeHandler}
-             checked={checked} onKeyDown={onKeyUpHandler}
-             className={inputClassName}
+             onKeyUp={onKeyUpHandler}
+             onBlur={onBlurHandler}
+             className={inputFinalClassName}
              placeholder={placeholder}
+             autoFocus={true}
       />
   );
 };
