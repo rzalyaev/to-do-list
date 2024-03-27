ad8d85c2b6794330537f42f316f37c957e016412
rzalyaev <ruslan.zalyaev@gmail.com>
2024-03-27 21:40:29 +0400

    Create App stories

---
+++ src/App.stories.tsx
@@ -0,0 +1,22 @@
+import type {Meta, StoryObj} from '@storybook/react';
+import App from "./App";
+import {StoreProviderDecorator} from "./stories/decorators/StoreProviderDecorator";
+
+// More on how to set up stories at:
+// https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta: Meta<typeof App> = {
+  title: 'TODOLISTS/App',
+  component: App,
+  // This component will have an automatically generated Autodocs entry:
+  // https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ['autodocs'],
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  decorators: [StoreProviderDecorator]
+};
+
+export default meta;
+type Story = StoryObj<typeof App>;
+
+// More on component templates:
+// https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+export const AppStory: Story = {}
